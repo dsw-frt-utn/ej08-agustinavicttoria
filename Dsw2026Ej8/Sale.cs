5d3457cea160d31412a488baeb28b1a0d8eed2d6
agustinavicttoria <mariel.victtoria@gmail.com>
2026-07-01 20:29:28 -0300

    Problema 6: extensión ToProductCode y clase Problema6

---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Sale
+    {
+        public decimal Amount { get; set; }
+        public Sale(decimal amount)
+        {
+            Amount = amount;
+        }
+        public virtual decimal CalculateTotal()
+        {
+            return Amount;
+        }
+    }
+}
